7b1f0c9581b7cba150402610398a4fe5165ff691
Greg DeLozier <gregdelozier@gmail.com>
2024-02-21 19:37:54 -0500

    ball size change complete

--- sketch.js
+++ sketch.js
@@ -4,11 +4,20 @@ function setup() {
   y = 100
   dx = 1
   dy = 1
+  size = 85
+  ds = 0.25
 }
 
 function draw() {
   background(220);
-  circle(x,y,75)
+  circle(x,y,size)
+  size = size + ds
+  if (size > 95) {
+    ds = -0.25
+  }
+  if (size < 75) {
+    ds = .25
+  }
   if (x > 300 | x < 100) {
     dx = - dx
   }
